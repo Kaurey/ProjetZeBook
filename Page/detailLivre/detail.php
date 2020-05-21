56d21f4d19ec061f003a8968a2179f8d2ba3a499
Kaurey <corentinbouet77120@gmail.com>
2020-05-21 21:13:45 +0200

    Update page concernant les détails

--- Page/detailLivre/detail.php
+++ Page/detailLivre/detail.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Détail du Livre - ZeBook</title>
-		<link rel="stylesheet" type="text/css" href="../../menu.css">
+		<link rel="stylesheet" type="text/css" href="../menu.css">
 		<link rel="stylesheet" type="text/css" href="detail.css">
 	</head>
 
@@ -18,9 +18,9 @@
 
 		<?php include("../baseDonnées.php");
 
-		$parametre_isbn = $_GET['isbn'];
+		$isbn = $_GET['isbn'];
 
-		$detail = $bdd->query('SELECT isbn, titre, nom, prenom, role.libelle AS libelle_role, editeur.libelle AS libelle_editeur, genre.libelle AS libelle_genre, 											langue.libelle AS libelle_langue , nbpages, annee
+		$detail = $bdd->prepare('SELECT isbn, titre, nom, prenom, role.libelle AS libelle_role, editeur.libelle AS libelle_editeur, genre.libelle AS libelle_genre, 											langue.libelle AS libelle_langue , nbpages, annee
 							   FROM livre
 							   JOIN editeur ON livre.editeur = editeur.id
 							   JOIN auteur ON livre.isbn = auteur.idLivre
@@ -28,15 +28,49 @@
 							   JOIN genre ON livre.genre = genre.id
 							   JOIN langue ON livre.langue = langue.id
 							   JOIN role ON auteur.idRole = role.id
-							   WHERE isbn = $parametre_isbn'); // Je n'ai pas réussi à trouver comment insérer une variable dans l'isbn malgré mes recherches sur internet
+							   WHERE isbn = :isbn');
+		$detail->execute(array(
+           	'isbn' => $isbn));
 
-		 ?>
+		while($donnee = $detail->fetch())
+		 	{
+		 	?>	
 
-		 <?php include("../baseDonnées");?>
+		 	<img src="../images/<?php echo $donnee['isbn'];?>.jpg">
 
+		 	<p>
+		 		Titre : <?php echo $donnee['titre'];?> <br>
+		 	   
+		 	   	Auteur : <?php echo $donnee['prenom'] . " " . $donnee['nom'];?> <br>
+		 	   
+		 	   	Rôle : <?php echo $donnee['libelle_role'];?> <br>
+		 	   
+		 	   	Editeur : <?php echo $donnee['libelle_editeur'];?> <br>
+		 	   
+		 	   	Genre : <?php echo $donnee['libelle_genre'];?> <br>
+		 	   
+		 	   	Langue : <?php echo $donnee['libelle_langue'];?><br>
+		 	   
+		 	   	Nombre de pages : <?php
+		 	   
+		 	   	if($donnee['nbpages'] = "NULL"){
+		 	   		echo "Inconnu";
+		 	   	}else{
+		 	   		echo $donnee['nbpages'];
+		 	   	}?> <br>
+		 	   
+		 	   	Année de publication : <?php echo $donnee['annee'];?>
+		 	</p>
 
-		 
-		
+		 	
+		 	<?php 
+
+			}
+
+			$detail->closeCursor();
+
+			?>
+			<input type="submit" name="panier" value="Ajouter au panier">
 
 	</body>
 </html>
