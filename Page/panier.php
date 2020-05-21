171c56c340e96b038e8d97d5ceae8d818f56240b
Kaurey <corentinbouet77120@gmail.com>
2020-05-21 21:14:35 +0200

    Création panier

---
+++ Page/panier.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="menu.css">
+		<link rel="stylesheet" type="text/css" href="panier.css">
+	</head>
+	<body>
+
+		<header>
+
+			<!-- Bannière et menu du site -->
+		
+			<?php include("menu.php"); ?>
+	
+		</header>
+
+		<h1 class="titre_panier">Votre panier</h1>
+
+	</body>
+</html>
