e61c5b4787bf13571244114f7384852c00abfd4e
Kaurey <corentinbouet77120@gmail.com>
2020-05-22 22:12:18 +0200

    Modification page detail

---
+++ Page/detail.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Détail du Livre - ZeBook</title>
+		<link rel="stylesheet" type="text/css" href="menu.css">
+		<link rel="stylesheet" type="text/css" href="detail.css">
+	</head>
+
+	<header>
+
+		<!-- Bannière et menu du site -->
+		
+		<?php include("menu.php"); ?>
+	
+	</header>
+
+	<body>
+
+		<?php include("baseDonnées.php");
+
+		$isbn = $_GET['isbn'];
+
+		$detail = $bdd->prepare('SELECT isbn, titre, nom, prenom, role.libelle AS libelle_role, editeur.libelle AS libelle_editeur, genre.libelle AS libelle_genre, 											langue.libelle AS libelle_langue , nbpages, annee
+							   FROM livre
+							   JOIN editeur ON livre.editeur = editeur.id
+							   JOIN auteur ON livre.isbn = auteur.idLivre
+							   JOIN personne ON auteur.idPersonne = personne.id
+							   JOIN genre ON livre.genre = genre.id
+							   JOIN langue ON livre.langue = langue.id
+							   JOIN role ON auteur.idRole = role.id
+							   WHERE isbn = :isbn');
+		$detail->execute(array(
+           	'isbn' => $isbn));
+
+		while($donnee = $detail->fetch())
+		 	{
+		 	?>	
+
+		 	<img src="images/<?php echo $donnee['isbn'];?>.jpg">
+
+		 	<p>
+		 		Titre : <?php echo $donnee['titre'];?> <br>
+		 	   
+		 	   	Auteur : <?php echo $donnee['prenom'] . " " . $donnee['nom'];?> <br>
+		 	   
+		 	   	Rôle : <?php echo $donnee['libelle_role'];?> <br>
+		 	   
+		 	   	Editeur : <?php echo $donnee['libelle_editeur'];?> <br>
+		 	   
+		 	   	Genre : <?php echo $donnee['libelle_genre'];?> <br>
+		 	   
+		 	   	Langue : <?php echo $donnee['libelle_langue'];?><br>
+		 	   
+		 	   	Nombre de pages : <?php
+		 	   
+		 	   	if($donnee['nbpages'] = "NULL"){
+		 	   		echo "Inconnu";
+		 	   	}else{
+		 	   		echo $donnee['nbpages'];
+		 	   	}?> <br>
+		 	   
+		 	   	Année de publication : <?php echo $donnee['annee'];?>
+		 	</p>
+
+		 	
+		 	<?php 
+
+			}
+
+			
+			if(!empty($_SESSION['id'])){?>
+			<p class="ajouter"><a href="livrepanier.php?isbn=<?php echo $_GET['isbn']?>&idMembre=<?php echo $_SESSION['id']?>">Ajouter au panier</a></p>
+			
+			<?php
+			}
+			?>
+	</body>
+</html>
